01127ab0dadda5e1660daf775aa35bfd9528debf
Ruslan Suvorov <ruslanvs@yahoo.com>
2020-12-30 21:16:26 -0800

    Basic progress view

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ruslan Suvorov on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
