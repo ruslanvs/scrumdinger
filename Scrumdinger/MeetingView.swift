c13068aca1a4d1dc68eb441cc9dc8d4a0c09a4fb
Ruslan Suvorov <ruslanvs@yahoo.com>
2020-12-30 21:44:46 -0800

    MeetingView prototype

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -12,16 +12,33 @@ struct MeetingView: View {
         VStack {
             ProgressView(value: 5, total: 15)
             HStack {
-                VStack {
+                VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
+                        .font(.caption)
                     Label("300", systemImage: "hourglass.bottomhalf.fill")
                 }
-                VStack {
+                Spacer()
+                VStack(alignment: .trailing) {
                     Text("Seconds Remaining")
+                        .font(.caption)
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel(Text("Time Remaining"))
+            .accessibilityValue(Text("10 minutes"))
+            Circle()
+                .strokeBorder(lineWidth: 24, antialiased: true)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
+            .accessibilityLabel(Text("Next speaker"))
         }
+        .padding()
     }
 }
 
