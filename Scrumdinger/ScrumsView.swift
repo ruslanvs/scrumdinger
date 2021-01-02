90cd68d048cf70de8e074f5f02b0a67c42aafa2b
Ruslan Suvorov <ruslanvs@yahoo.com>
2021-01-02 15:49:58 -0800

    Persistence with FileManager

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
+    @Environment(\.scenePhase) private var scenePhase
     @State var isPresented = false
     @State var newScrumData = DailyScrum.Data()
+    let saveAction: () -> Void
     var body: some View {
         List {
             ForEach (scrums) { scrum in
@@ -37,6 +39,9 @@ struct ScrumsView: View {
                 })
             }
         }
+        .onChange(of: scenePhase) { phase in
+            if phase == .inactive { saveAction() }
+        }
     }
     
     private func binding(for scrum: DailyScrum) -> Binding<DailyScrum> {
@@ -50,7 +55,7 @@ struct ScrumsView: View {
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            ScrumsView(scrums: .constant(DailyScrum.data))
+            ScrumsView(scrums: .constant(DailyScrum.data), saveAction: {})
         }
     }
 }
