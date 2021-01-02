5d3a037ba027c4dce9106566853eae3f8160e491
Ruslan Suvorov <ruslanvs@yahoo.com>
2021-01-01 21:09:31 -0800

    Adding to scrum history

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -8,19 +8,21 @@
 
 import SwiftUI
 
-struct DailyScrum: Identifiable {
+struct DailyScrum: Identifiable, Codable {
     let id: UUID
     var title: String
     var attendees: [String]
     var lengthInMinutes: Int
     var color: Color
-    
-    init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, color: Color) {
+    var history: [History]
+
+    init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, color: Color, history: [History] = []) {
         self.id = id
         self.title = title
         self.attendees = attendees
         self.lengthInMinutes = lengthInMinutes
         self.color = color
+        self.history = history
     }
 }
 
@@ -41,11 +43,11 @@ extension DailyScrum {
         var lengthInMinutes: Double = 5.0
         var color: Color = .random
     }
-    
+
     var data: Data {
         return Data(title: title, attendees: attendees, lengthInMinutes: Double(lengthInMinutes), color: color)
     }
-    
+
     mutating func update(from data: Data) {
         title = data.title
         attendees = data.attendees
